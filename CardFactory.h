932fbc28592bfda6ac7af126b35d76481861cd24
Eden Kindja Nehema <91163189+enehe074@users.noreply.github.com>
2021-12-03 15:55:19 -0500

    Update CardFactory.h

--- CardFactory.h
+++ CardFactory.h
@@ -1,5 +1,33 @@
+#ifndef CardFactory_h
+#define CardFactory_h
+
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include <chrono>
+#include "Deck.h"
+#include "Card.h"
+#include "Amethyst.cpp"
+#include "Emerald.cpp"
+#include "Hematite.cpp"
+#include "Malachite.cpp"
+#include "Obsidian.cpp"
+#include "Quartz.cpp"
+#include "Ruby.cpp"
+#include "Turquoise.cpp"
+
+class Deck;
+
 class CardFactory {
+private:
+	Deck *deck;
+	CardFactory(const CardFactory&);
+	CardFactory();
 
-
-
+public:
+	static CardFactory* getFactory();
+	Deck getDeck();
+	Card* getCard(char);
+	Deck setDeck (istream &);
 };
+#endif
